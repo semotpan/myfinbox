ab91b955cecd37da076257d9718de23ebe073d55
Serghei Motpan <semotpan@gmail.com>
2024-02-20 21:35:31 +0200

    Server CI GitHub Actions setup

--- server/src/test/java/io/myfinbox/ServerApplicationTests.java
+++ server/src/test/java/io/myfinbox/ServerApplicationTests.java
@@ -3,11 +3,10 @@ package io.myfinbox;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TestServerApplication.class)
 class ServerApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
