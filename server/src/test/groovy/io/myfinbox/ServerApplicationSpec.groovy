8952704fcff8001ea91e972cccf92975fbe5fc38
Serghei Motpan <semotpan@gmail.com>
2024-02-20 21:52:23 +0200

    Reorder dependency by category, add base utility dependencies, setup spock-groovy for testing

---
+++ server/src/test/groovy/io/myfinbox/ServerApplicationSpec.groovy
@@ -0,0 +1,22 @@
+package io.myfinbox
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import spock.lang.Specification
+import spock.lang.Tag
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+
+@Tag("integration")
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = TestServerApplication)
+class ServerApplicationSpec extends Specification {
+
+    @Autowired
+    ApplicationContext applicationContext
+
+    def "should load contexts"() {
+        expect:
+        assert applicationContext != null
+    }
+}
