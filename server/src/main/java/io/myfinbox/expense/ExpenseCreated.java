dceae1f79f4f0eaa9142bbd3b4496fe53f1ac1a9
Serghei Motpan <semotpan@gmail.com>
2024-03-23 17:56:04 +0200

    Expose REST API for create an expense

---
+++ server/src/main/java/io/myfinbox/expense/ExpenseCreated.java
@@ -0,0 +1,29 @@
+package io.myfinbox.expense;
+
+import io.myfinbox.shared.DomainEvent;
+import io.myfinbox.shared.PaymentType;
+import lombok.Builder;
+
+import javax.money.MonetaryAmount;
+import java.time.LocalDate;
+import java.util.UUID;
+
+import static io.myfinbox.shared.Guards.notNull;
+
+@Builder
+public record ExpenseCreated(UUID expenseId,
+                             UUID accountId,
+                             UUID categoryId,
+                             MonetaryAmount amount,
+                             LocalDate expenseDate,
+                             PaymentType paymentType) implements DomainEvent {
+
+    public ExpenseCreated {
+        notNull(expenseId, "expenseId cannot be null.");
+        notNull(accountId, "accountId cannot be null.");
+        notNull(categoryId, "categoryId cannot be null.");
+        notNull(amount, "amount cannot be null.");
+        notNull(expenseDate, "expenseDate cannot be null.");
+        notNull(paymentType, "paymentType cannot be null.");
+    }
+}
